d9495048d013bfcd15d561a68ec6bcb6375ea840
Esther Mkanyika <thecodingmkanyi@gmail.com>
2023-03-19 12:32:03 -0700

    A program to print a line in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle");
+/* A program to print "Programming is like building a multilingual puzzle"
+ */
+#include <stdio.h>
+
+/*This is the main method of the program
+ * We are using puys function to print the line
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
