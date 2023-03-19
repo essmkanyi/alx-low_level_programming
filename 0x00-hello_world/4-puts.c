411aeabb50865dcc564f2181d6e671d7e3492132
Esther Mkanyika <thecodingmkanyi@gmail.com>
2023-03-19 13:10:08 -0700

    A program to print a line in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
-/* This is a program to print a single line using the puts function.
- */
 #include <stdio.h>
 
-/** main - this is the main method. 
- * The line should print the line followed by a new line
- * The program should return 0
+/**
+ * main - Prints "Programming is like building a multilingual puzzle,
+ *	followed by a new line.
+ *
+ * Return zero after printing
  */
-int main (void)
+
+int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
